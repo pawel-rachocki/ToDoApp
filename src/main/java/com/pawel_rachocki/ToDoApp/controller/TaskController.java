3c696cf3024408de2e8197fc4b63e2bbd97b94e3
Pawel <pawel.logo@gmail.com>
2025-03-31 21:47:12 +0200

    Added GetMaping to TaskController

--- src/main/java/com/pawel_rachocki/ToDoApp/controller/TaskController.java
+++ src/main/java/com/pawel_rachocki/ToDoApp/controller/TaskController.java
@@ -1,2 +1,26 @@
-package com.pawel_rachocki.ToDoApp.controller;public class TaskController {
+package com.pawel_rachocki.ToDoApp.controller;
+
+import com.pawel_rachocki.ToDoApp.models.Task;
+import com.pawel_rachocki.ToDoApp.services.TaskService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public String getTasks(Model model){
+        List<Task> tasks = taskService.getAllTasks();
+        model.addAttribute("tasks", tasks);
+        return "tasks";
+
+    }
 }
