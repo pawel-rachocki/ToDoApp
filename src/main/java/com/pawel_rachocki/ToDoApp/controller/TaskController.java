294f42779c663f8cf3094fe702c5319c2251acab
Pawel <pawel.logo@gmail.com>
2025-03-31 21:46:06 +0200

    Added tasks.html ThymLeaf template

---
+++ src/main/java/com/pawel_rachocki/ToDoApp/controller/TaskController.java
@@ -0,0 +1,2 @@
+package com.pawel_rachocki.ToDoApp.controller;public class TaskController {
+}
---
+++ src/main/java/com/pawel_rachocki/ToDoApp/services/TaskService.java
@@ -0,0 +1,7 @@
+package com.pawel_rachocki.ToDoApp.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+}
