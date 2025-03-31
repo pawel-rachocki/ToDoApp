820400a5eb968946f3d7ee263cf53d9197c8be14
Pawel <pawel.logo@gmail.com>
2025-03-31 21:47:44 +0200

    Mod

--- src/main/java/com/pawel_rachocki/ToDoApp/services/TaskService.java
+++ src/main/java/com/pawel_rachocki/ToDoApp/services/TaskService.java
@@ -1,7 +1,20 @@
 package com.pawel_rachocki.ToDoApp.services;
 
+import com.pawel_rachocki.ToDoApp.models.Task;
+import com.pawel_rachocki.ToDoApp.repository.TaskRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class TodoService {
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
 }
